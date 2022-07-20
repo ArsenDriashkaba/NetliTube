6979fb427165498e8f7f3b51de3b8bd4f58c2873
ArsenDriashkaba <arsen.dryashkaba.super@gmail.com>
2022-07-20 14:46:16 +0200

    F1: Very Basic architecture of website

---
+++ components/Nav/Nav.js
@@ -0,0 +1,5 @@
+const Nav = () => {
+  return <nav>Hello its navigation</nav>;
+};
+
+export default Nav;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Footer from "../components/Footer/Footer";
+import Nav from "../components/Nav/Nav";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
